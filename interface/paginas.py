65f059188e4b0b123e3760eca60955bbfcb48069
Paulo <pauloqneto@gmail.com>
2025-03-30 20:36:54 -0300

    Ajute final da lógica de gerenciamento paginas

--- interface/paginas.py
+++ interface/paginas.py
@@ -1,5 +1,4 @@
 import flet as ft
-from formulario.transacao import FormularioTransacao
 
 class GerenciadorPaginas:
     def __init__(self, page: ft.Page):
@@ -37,7 +36,7 @@ class GerenciadorPaginas:
         botao = ft.ElevatedButton(
             text="Novo Formulário",
             on_click=lambda e:self._chama_formulario(), # chama o Método 2   
-            color=ft.Colors.BLACK38
+            color=ft.Colors.BLACK38,
         )
 
         self.page.clean()
@@ -56,6 +55,59 @@ class GerenciadorPaginas:
         )
         self.page.update()
     
+
     def _chama_formulario(self):
-        FormularioTransacao._formulario_ui(self)
+        self.page.clean()
+
+        self.comp_descricao = ft.TextField(
+            label="Descrição",
+        )
+
+        self.comp_qt = ft.Container(
+            alignment=ft.alignment.center,
+            expand=True,
+            padding=0,
+            content=ft.Row(
+                controls=[
+                    ft.Text(value='1', size=20, color=ft.Colors.WHITE),
+                    ft.Column(
+                        controls=[
+                            ft.Icon(name=ft.Icons.EXPAND_LESS, size=30, color=ft.Colors.WHITE38),
+                            ft.Icon(name=ft.Icons.EXPAND_MORE, size=30, color=ft.Colors.WHITE38),
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        spacing=0,
+                    )
+                ],
+                alignment=ft.alignment.center
+            )
+        )
+
+        # Botões
+        botoes = ft.Row(
+            controls=[
+                #ft.ElevatedButton("Salvar", on_click=self._salvar_formulario),
+                ft.ElevatedButton("Voltar", on_click=lambda e: self._pagina_inicial())
+            ],
+            alignment=ft.MainAxisAlignment.END
+        )
+
+        
+
+        formulario = ft.Column(
+            controls=[  # Lista de controles diretamente aqui
+                ft.Row(
+                    controls=[self.comp_descricao, self.comp_qt],
+                    alignment=ft.MainAxisAlignment.SPACE_AROUND
+                ),
+                ft.Container(expand=True),  # Espaço vazio simplificado
+                botoes
+            ],
+            expand=True,
+            spacing=20,
+        )
+
+        self.page.add(formulario)  
+
+        self.page.update()
             
